6299fb694f5b4bb3ed32ac4a3cc7e98e4f8b5398
Beth Hansen and Esti Shay <esti@>
2017-07-03 14:32:20 -0700

    create project for pop culture robot quiz.

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+
+// $(document).ready(function() {
+//   $(".clickable").click(function() {
+//     $("img").fadeIn();
+//     });
+//   });
+
+  $(document).ready(function() {
+    $(".rosie").click(function() {
+      $("img").fadeIn();
+      });
+    });
+
+  $(document).ready(function() {
+    $(".b9").click(function() {
+      $("img").fadeIn();
+      });
+    });
+
+  $(document).ready(function() {
+    $(".robby").click(function() {
+      $("img").fadeIn();
+      });
+    });
+
+$(document).ready(function() {
+  $(".rosie").click(function() {
+    $(".rosie-showing").fadeToggle();
+    $(".rosie-hidden").fadeToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".b9").click(function() {
+    $(".b9-showing").fadeToggle();
+    $(".b9-hidden").fadeToggle();
+  });
+});
+//
+$(document).ready(function() {
+  $(".robby").click(function() {
+    $(".robby-showing").fadeToggle();
+    $(".robby-hidden").fadeToggle();
+  });
+});
+
+// $(document).ready(function() {
+//   $(".clickable").click(function() {
+//     $(".initially-showing").fadeToggle();
+//     $(".initially-hidden").fadeToggle();
+//   });
+// });
