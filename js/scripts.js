5a51fcd530cf20194ea6142d78074365b4202d7f
Beth Hansen and Esti Shay <esti@>
2017-07-03 16:04:57 -0700

    add borders to images and refactor the JS to be more DRY.

--- js/scripts.js
+++ js/scripts.js
@@ -9,93 +9,51 @@ $(document).ready(function() {
   $(".rosie").click(function() {
     $("img").fadeIn();
     });
-  });
-
-$(document).ready(function() {
   $(".b9").click(function() {
     $("img").fadeIn();
     });
-  });
-
-$(document).ready(function() {
   $(".robby").click(function() {
     $("img").fadeIn();
     });
-  });
-
-$(document).ready(function() {
   $(".johnny5").click(function() {
     $("img").fadeIn();
     });
-  });
-
-$(document).ready(function() {
   $(".data").click(function() {
     $("img").fadeIn();
     });
-  });
-
-$(document).ready(function() {
   $(".chappie").click(function() {
     $("img").fadeIn();
     });
-  });
-
-$(document).ready(function() {
   $(".rosie").click(function() {
     $(".rosie-showing").fadeToggle();
     $(".rosie-hidden").fadeToggle();
-  });
-});
-
-$(document).ready(function() {
+   });
   $(".b9").click(function() {
     $(".b9-showing").fadeToggle();
     $(".b9-hidden").fadeToggle();
   });
-});
-
-$(document).ready(function() {
   $(".robby").click(function() {
     $(".robby-showing").fadeToggle();
     $(".robby-hidden").fadeToggle();
   });
-});
-
-$(document).ready(function() {
   $(".johnny5").click(function() {
     $(".johnny5-showing").fadeToggle();
     $(".johnny5-hidden").fadeToggle();
   });
-});
-
-$(document).ready(function() {
   $(".data").click(function() {
     $(".data-showing").fadeToggle();
     $(".data-hidden").fadeToggle();
   });
-});
-
-$(document).ready(function() {
   $(".chappie").click(function() {
     $(".chappie-showing").fadeToggle();
     $(".chappie-hidden").fadeToggle();
   });
-});
-
-$(document).ready(function () {
   $("button#dark").click(function() {
     $("body").addClass("dark-background");
   });
-});
-
-$(document).ready(function () {
   $("button#light").click(function() {
     $("body").removeClass();
   });
-});
-
-$(document).ready(function() {
   $(".thanks").click(function() {
     $("h4").addClass("colored-box");
     });
